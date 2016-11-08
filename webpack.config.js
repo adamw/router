6b20b44f6f58f72103af7bc1f9ff01d3e7115ae5
adamw <adam@warski.org>
2016-11-08 11:27:55 +0100

    Build updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,3 @@
-var path = require('path');
-
 module.exports = {
   entry: "./app.js",
   output: {
@@ -24,13 +22,15 @@ module.exports = {
         query: {
           psc: 'psa',
           src: ['bower_components/purescript-*/src/**/*.purs', 'src/**/*.purs'],
-          pscArgs: { sourceMaps: true }
+          pscArgs: { sourceMaps: true },
+          bundle: false
         }
       }
     ]
   },
   // http://stackoverflow.com/questions/30870830/how-do-i-generate-sourcemaps-when-using-babel-and-webpack
   devtool: 'source-map',
+  devserver: { stats: 'errors-only' },
   resolve: {
     modulesDirectories: ['node_modules', 'bower_components'],
     extensions: [ '', '.js', '.purs']
